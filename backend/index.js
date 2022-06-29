d3c8e090dac1826f4589d1a3e5c5b6c500e29e2a
AbhiJ2706 <43829700+AbhiJ2706@users.noreply.github.com>
2022-06-28 22:21:23 -0400

    finalizing the docker images

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
-const cors = require('cors');
-app.use(cors({origin: '*', credentials: false}));
-app.use(function (req, res, next) {	
-    res.setHeader('Access-Control-Allow-Origin', '*');    
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With,content-type, Content-Type, Accept');   
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+
 
 app.use(express.json()) 
 app.use(require("./routes/eval"))
+
+app.get("/hello",  async(req,res) => {
+    res.send("hello!")
+});
+
+app.get("/",  async(req,res) => {
+    res.send("ok!")
+});
 
 app.listen(PORT, ()=>{
     console.log("Port: " +  PORT)
